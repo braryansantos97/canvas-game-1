9ffb24cac67e0bdb4c16394bbaf6f9b6c458c7f7
Brayan Santos <santos.a.brayan97@gmail.com>
2022-04-19 00:28:23 -0400

    index.js updated, collisions detected

--- index.js
+++ index.js
@@ -75,16 +75,18 @@ const enemies = [];
 
 function spawnEnemies() {
   setInterval(() => {
-    const radius = 20
+    const radius = Math.random() * (30 -4) + 4
 
     let x
     let y
     if (Math.random() < 0.5) {
-
      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
-     y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+     y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
     }
-    
+
     const color = 'green'
 
     const angle = Math.atan2(
@@ -101,16 +103,36 @@ function spawnEnemies() {
   }, 1000)
 }
 
+let animationId
 function animate() {
-  requestAnimationFrame(animate);
+  animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
   })
 
-  enemies.forEach((enemy) => {
+  enemies.forEach((enemy, index) => {
     enemy.update();
+
+    //end game
+    const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+    if (distance - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId)
+    }
+
+
+    projectiles.forEach( (projectile, projectileIndex) => {
+      const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+      //enemy and projectile touch
+      if (distance - enemy.radius - projectile. radius < 1) {
+        setTimeout(() => {
+          enemies.splice(index, 1)
+          projectiles.splice(projectileIndex, 1)    
+        }, 0);
+      }
+    })
   })
 }
 
